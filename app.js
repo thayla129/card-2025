18de6c3b13fdecf4a160d7e7c12c7ebfabf14356
THAYLA AMORIM MATEUS <24122553@CFP127ALUNO.LOCAL>
2025-02-13 16:43:16 -0300

    Estrutura HTML pronta, CSS e CARDS prontos, se haver algum tipo de mudança será feito o processo de novo

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-'use strict'
+'use strict';
 
-const categorias =[
-    {nome: 'Celulares', icon: 'telefone.png', cor: 'red'},
-    {nome:'Tenis',icon: 'tenis.png', cor: 'blue'},
-    {nome:'Televisão Retro', icon: 'tvretro.png', cor: 'pink'},
-    {nome:'Televisão', icon: 'tv.png', cor: 'blue'}
+const categorias = [
+    { nome: 'Mobile', icon: 'iconmobile.png', cor: 'white' },
+    { nome: 'Shoes', icon: 'iconshoes.png', cor: 'white' },
+    { nome: 'SmartWatch', icon: 'iconsmartwatch.png', cor: 'white' },
+    { nome: 'HeadPhone', icon:'iconheadphones.png', cor: 'white' }
+];
 
-]
-function criarMenu(categoria){
-    const novoItem = document.createElement('li')
-    const novaImagem = document.createElement('img')
-    const novoSpan = document.createElement('span')
-    const lista = document.getElementById('menu')
+function criarMenu(categoria) {
+    const novoItem = document.createElement('li');
+    const novaImagem = document.createElement('img');
+    const novoSpan = document.createElement('span');
+    const lista = document.getElementById('menu');
 
-    novaImagem.src = `./img/${categoria.icon}`
-    novoSpan.textContent = categoria.nome
-    novoItem.style = `--cor-hover: ${categoria.cor}`
+    novaImagem.src = `./img/${categoria.icon}`;
+    novaImagem.alt = categoria.nome; 
+    novaImagem.loading = "lazy"; 
+    novoSpan.textContent = categoria.nome;
+    novoItem.style = `--cor-hover: ${categoria.cor}`;
 
-    novoItem.appendChild(novaImagem)
-    novoItem.appendChild(novoSpan)
-
-    lista.appendChild(novoItem)
-
+    novoItem.appendChild(novaImagem);
+    novoItem.appendChild(novoSpan);
+    lista.appendChild(novoItem);
 }
-categorias.forEach(criarMenu)
 
-
-criarMenu()
+categorias.forEach(criarMenu);
